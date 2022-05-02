f140966f7f4ebe45eb00e55da56f25710671e0d1
Ramiro Serra <serraramiro@ekumenlabs.com>
2022-05-02 10:42:28 -0300

    linter

--- preprocessing/occupancy_grid/occupancy_grid.py
+++ preprocessing/occupancy_grid/occupancy_grid.py
@@ -1,38 +1,42 @@
-from dataclasses import dataclass
 from matplotlib import pyplot as plt
 import numpy as np
-import os
 from typing import *
+
 
 class OccupancyGrid:
     def __init__(self, cell_size: float, x_cells: int, y_cells: int) -> None:
         self._cell_size = cell_size
         self._x_cells = x_cells
         self._y_cells = y_cells
-        self._grid = np.zeros((x_cells, y_cells), dtype=np.int8) # grid containing ints between 0 and 100, representing % occupancy
+        self._grid = np.zeros(
+            (x_cells, y_cells), dtype=np.int8
+        )  # grid containing ints between 0 and 100, representing % occupancy
 
     def get_cell_size(self) -> float:
         return self._cell_size
+
     def get_x_cells(self) -> int:
         return self._x_cells
+
     def get_y_cells(self) -> int:
-        return self._y_cells 
+        return self._y_cells
+
     def get_grid(self) -> np.ndarray:
         return self._grid
+
     # gets the occupancy percentage given X and Y coordinates
     def __getitem__(self, index: Tuple[int, int]) -> int:
         return self._grid[index]
-    # sets the occupancy percentage given X and Y coordinates 
+
+    # sets the occupancy percentage given X and Y coordinates
     def __setitem__(self, index: Tuple[int, int], value: int) -> None:
         self._grid[index] = value
+
     def plot(self):
         # Create a new figure
-
         fig = plt.figure()
 
         ax = fig.gca()
 
         # Plot the grid
-        ax.pcolormesh(self.get_grid(), cmap='PuBu', edgecolor='k', lw=2)
-
-
+        ax.pcolormesh(self.get_grid(), cmap="PuBu", edgecolor="k", lw=2)
--- preprocessing/occupancy_grid/occupancy_grid_test.py
+++ preprocessing/occupancy_grid/occupancy_grid_test.py
@@ -3,14 +3,16 @@ import unittest
 import numpy as np
 from occupancy_grid import OccupancyGrid
 
-# define a testcase for occupancy grid
+
 class TestOccupancyGrid(unittest.TestCase):
     def testCreation(self):
         grid = OccupancyGrid(cell_size=0.1, x_cells=10, y_cells=10)
         self.assertEqual(grid.get_cell_size(), 0.1)
         self.assertEqual(grid.get_x_cells(), 10)
         self.assertEqual(grid.get_y_cells(), 10)
-        self.assertTrue(np.array_equal(grid.get_grid(), np.zeros((10, 10), dtype=np.int8)))
+        self.assertTrue(
+            np.array_equal(grid.get_grid(), np.zeros((10, 10), dtype=np.int8))
+        )
 
 
 if __name__ == "__main__":
